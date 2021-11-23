5527df941ba91bfff12029b66b78947eb3bacfbc
MTOBITT <tomoetbitt@gmail.com>
2021-11-23 10:41:53 +0000

    kadai-second

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -48,10 +48,11 @@ class TasksController < ApplicationController
     redirect_to tasks_url
   end
 
-end
+
 private
 
   # Strong Parameter
   def task_params
     params.require(:task).permit(:content)
   end
+end
